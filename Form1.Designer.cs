486890f15b61df76dfe9f18c4a9d574b35baa4c3
Alexander Shabalin <gessman@outlook.com>
2021-08-07 22:48:05 +0300

    First working version

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,111 @@ namespace _5words
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.Location = new System.Drawing.Point(625, 30);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(75, 23);
+            this.btn_Next.TabIndex = 17;
+            this.btn_Next.Text = "Next";
+            this.btn_Next.UseVisualStyleBackColor = true;
+            this.btn_Next.Click += new System.EventHandler(this.btn_Next_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(355, 179);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(345, 120);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(7, 179);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(345, 120);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(355, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(345, 120);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(7, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(345, 120);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(312, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 21);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(706, 304);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "5 words";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +18,191 @@ namespace _5words
             InitializeComponent();
         }
 
+        private string path;
+        public static int index_ = 0; // предыдущий индекс англ слова
+        public static int index = 0, index1 = 0, index2 = 0, index3 = 0, index4 = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            path = Environment.CurrentDirectory;
+
+            try
+            {
+                using (FileStream fstream = File.OpenRead($"{path}\\database.txt"))
+                {
+                    // преобразуем строку в байты
+                    byte[] array = new byte[fstream.Length];
+                    // считываем данные
+                    fstream.Read(array, 0, array.Length);
+                    // декодируем байты в строку
+                    string textFromFile = System.Text.Encoding.UTF8.GetString(array);
+
+                    string[] words = textFromFile.Split(new char[] { ';' });
+
+
+                    int i = 2;
+                    foreach (string item in words)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            if (item != "")
+                                words_rus.Add(item);
+                        }
+                        if (i % 2 != 0)
+                        {
+                            if (item != "")
+                                words_eng.Add(item);
+                        }
+                        i++;
+                    }
+
+                    indexes();
+                    label1.Text = words_eng[index];
+                    button1.Text = words_rus[index1]; button2.Text = words_rus[index2];
+                    button3.Text = words_rus[index3]; button4.Text = words_rus[index4];
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_Next_Click(object sender, EventArgs e)
+        {
+            indexes();
+            label1.Text = words_eng[index];
+            button1.Text = words_rus[index1]; button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            button2.Text = words_rus[index2]; button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            button3.Text = words_rus[index3]; button3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            button4.Text = words_rus[index4]; button4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+        }
+
+        public static List<string> words_rus = new List<string>();
+        public static List<string> words_eng = new List<string>();
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button; // b это кнопка, на которую кликаем
+
+            ((Button)sender).BackColor = SystemColors.ActiveCaption;
+
+            Button[] bt = new Button[4];
+            bt[0] = button1; bt[1] = button2; bt[2] = button3; bt[3] = button4;
+
+            int j = 0;
+            foreach (Button btns in bt)
+            {
+                if (bt[j] != (Button)sender)
+                {
+                    bt[j].BackColor = SystemColors.GradientInactiveCaption;
+                }
+                j++;
+            }
+
+            if (b.Text == words_rus[index])
+            {
+                //this.Text = b.Text;
+
+                ((Button)sender).BackColor = SystemColors.Highlight;
+
+            }
+            else { b.BackColor = SystemColors.ButtonShadow; }
+        }
+
+        public void indexes()
+        {
+            Random rnd = new Random();
+
+            index = rnd.Next(0, words_rus.Count);
+            if (index == index_)
+                while (index == index_)
+                    index = rnd.Next(0, words_rus.Count);
+
+            int this_index = rnd.Next(1, 5);
+
+            switch (this_index)
+            {
+                case 1:
+                    { index_ = index1 = index; }; break;
+                case 2:
+                    { index_ = index2 = index; }; break;
+                case 3:
+                    { index_ = index3 = index; }; break;
+                case 4:
+                    { index_ = index4 = index; }; break;
+            }
+
+            switch (this_index)
+            {
+                case 1: // index_ = index1 = index;
+                    {
+                        index2 = rnd.Next(0, words_rus.Count); // 2
+                        if ((index2 == index1) || (index2 == index_) || (index2 == index))
+                            while ((index2 == index1) || (index2 == index_) || (index2 == index))
+                                index2 = rnd.Next(0, words_rus.Count);
+
+                        index3 = rnd.Next(0, words_rus.Count); // 3
+                        if ((index3 == index1) || (index3 == index2) || (index3 == index4) || (index3 == index_) || (index3 == index))
+                            while ((index3 == index1) || (index3 == index2) || (index3 == index4) || (index3 == index_) || (index3 == index))
+                                index3 = rnd.Next(0, words_rus.Count);
+
+                        index4 = rnd.Next(0, words_rus.Count); // 4
+                        if ((index4 == index3) || (index4 == index2) || (index4 == index1) || (index4 == index_) || (index3 == index))
+                            while ((index4 == index3) || (index4 == index2) || (index4 == index1) || (index4 == index_) || (index3 == index))
+                                index4 = rnd.Next(0, words_rus.Count);
+                    }; break;
+                case 2: // index_ = index2 = index;
+                    {
+                        index1 = rnd.Next(0, words_rus.Count); // 1
+                        if ((index1 == index_) || (index1 == index))
+                            while ((index1 == index_) || (index1 == index))
+                                index1 = rnd.Next(0, words_rus.Count);
+
+                        index3 = rnd.Next(0, words_rus.Count); // 3
+                        if ((index3 == index1) || (index3 == index4) || (index3 == index_) || (index3 == index2) || (index3 == index))
+                            while ((index3 == index1) || (index3 == index4) || (index3 == index_) || (index3 == index2) || (index3 == index))
+                                index3 = rnd.Next(0, words_rus.Count);
+
+                        index4 = rnd.Next(0, words_rus.Count); // 4
+                        if ((index4 == index3) || (index4 == index2) || (index4 == index1) || (index4 == index) || (index4 == index_))
+                            while ((index4 == index3) || (index4 == index2) || (index4 == index1) || (index4 == index) || (index4 == index_))
+                                index4 = rnd.Next(0, words_rus.Count);
+                    }; break;
+                case 3: // index_ = index3 = index;
+                    {
+                        index1 = rnd.Next(0, words_rus.Count); // 1
+                        if ((index1 == index3) || (index1 == index) || (index1 == index_))
+                            while ((index1 == index3) || (index1 == index) || (index1 == index_))
+                                index1 = rnd.Next(0, words_rus.Count);
+
+                        index2 = rnd.Next(0, words_rus.Count); // 2
+                        if ((index2 == index1) || (index2 == index3) || (index2 == index) || (index2 == index_))
+                            while ((index2 == index1) || (index2 == index3) || (index2 == index) || (index2 == index_))
+                                index2 = rnd.Next(0, words_rus.Count);
+
+                        index4 = rnd.Next(0, words_rus.Count); // 4
+                        if ((index4 == index3) || (index4 == index2) || (index4 == index1) || (index4 == index) || (index4 == index_))
+                            while ((index4 == index3) || (index4 == index2) || (index4 == index1) || (index4 == index) || (index4 == index_))
+                                index4 = rnd.Next(0, words_rus.Count);
+                    }; break;
+                case 4: // index_ = index4 = index;
+                    {
+                        index1 = rnd.Next(0, words_rus.Count); // 1
+                        if ((index1 == index_) || (index1 == index4) || (index1 == index))
+                            while ((index1 == index_) || (index1 == index4) || (index1 == index))
+                                index1 = rnd.Next(0, words_rus.Count);
+
+                        index2 = rnd.Next(0, words_rus.Count); // 2
+                        if ((index2 == index4) || (index2 == index) || (index2 == index_))
+                            while ((index2 == index4) || (index2 == index) || (index2 == index_))
+                                index2 = rnd.Next(0, words_rus.Count);
+
+                        index3 = rnd.Next(0, words_rus.Count); // 3
+                        if ((index3 == index1) || (index3 == index2) || (index3 == index4) || (index3 == index) || (index3 == index_))
+                            while ((index3 == index1) || (index3 == index2) || (index3 == index4))
+                                index3 = rnd.Next(0, words_rus.Count);
+                    }; break;
+            }
+        }
     }
 }
